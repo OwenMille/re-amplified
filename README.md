b57a0e8d9cb78e20feed5b3485b8fc4df6f2e49a
Owen Miller <90118261+OwenMille@users.noreply.github.com>
2022-06-02 15:47:28 -0400

    wrap auth

--- README.md
+++ README.md
@@ -1,7 +1,33 @@
+##### Combination of two tutorials
+
+[AWS Tuorial React App with Amplify](https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/module-one/)
+
 [Amplify Docs React App Tutorial](https://docs.amplify.aws/start/getting-started/setup/q/integration/react/#initialize-a-new-backend)
 
-useEffect - When the component loads, the useEffect hook is called and it invokes the fetchTodos function.
+## Auth
 
-fetchTodos - Uses the Amplify API category to call the AppSync GraphQL API with the listTodos query. Once the data is returned, the items array is passed in to the setTodos function to update the local state.
+[AmplifyUI Authenticator customization](https://ui.docs.amplify.aws/react/components/authenticator#customization)
 
-addTodo - Uses the Amplify API category to call the AppSync GraphQL API with the createTodo mutation. A difference between the listTodos query and the createTodo mutation is that createTodo accepts an argument containing the variables needed for the mutation.
+## Deployment
+
+[Continuous Deployment](https://aws.amazon.com/getting-started/hands-on/build-react-app-amplify-graphql/module-one/)
+
+![Front End Amplify Deploy Source Module 1](https://d1.awsstatic.com/webteam/getting_started/GSRC%202020%20updates/Front%20End/Front%20End%20Amplify%20Deploy%20Source%20Module%201.00becc211a8ecd42349cffb87406449074ed2e5c.png)
+
+#### ALTERNATIVE: manual deploy and host
+
+[Publish your app](https://docs.amplify.aws/start/getting-started/hosting/q/integration/react/#publish-your-app)
+
+Run the following command to publish your app.
+
+```bash
+amplify publishcopy
+```
+
+After publishing, your terminal will display your app URL hosted on a `amplifyapp.com` domain. Whenever you have additional changes to publish, just re-run the `amplify publish` command.
+
+You may get an "AccessDenied" error if your app's distribution directory is not set properly. To fix this, change the distribution directory via `amplify configure project` and then re-run `amplify publish`.
+
+To view your app and hosting configuration in the Amplify Console, run the `amplify console` command.
+
+Note: In order to delete all the environments of the project from the cloud and wipe out all the local files created by Amplify CLI, run `amplify delete` command. Now, to observe that your Amplify project resources have been deleted, run `amplify status` command.
--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 /* src/App.js */
 import React, { useEffect, useState } from 'react'
 import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import awsExports from "./aws-exports";
 import { createTodo } from './graphql/mutations'
 import { listTodos } from './graphql/queries'
+import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 
-import awsExports from "./aws-exports";
 Amplify.configure(awsExports);
-
 const initialState = { name: '', description: '' }
 
-const App = () => {
+function App({ signOut, user }) {
+
+
   const [formState, setFormState] = useState(initialState)
   const [todos, setTodos] = useState([])
 
@@ -42,47 +45,45 @@ const App = () => {
   }
 
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <div style={styles.container}>
-          <h1>Hello {user.username}</h1>
-          <button style={styles.button} onClick={signOut}>Sign out</button>
-          <br />
-          <h2>Amplify Todos</h2>
-          <input
-            onChange={event => setInput('name', event.target.value)}
-            style={styles.input}
-            value={formState.name}
-            placeholder="Name"
-          />
-          <input
-            onChange={event => setInput('description', event.target.value)}
-            style={styles.input}
-            value={formState.description}
-            placeholder="Description"
-          />
-          <button style={styles.button} onClick={addTodo}>Create Todo</button>
-          {
-            todos.map((todo, index) => (
-              <div key={todo.id ? todo.id : index} style={styles.todo}>
-                <p style={styles.todoName}>{todo.name}</p>
-                <p style={styles.todoDescription}>{todo.description}</p>
-              </div>
-            ))
-          }
-        </div>
-      )}
-    </Authenticator>
+    <div style={styles.container}>
+      <Heading level={1}>Hello {user.username}</Heading>
+      <Button onClick={signOut}>Sign out</Button>
+        <br />
+        <h2>Amplify Todos</h2>
+        <input
+          onChange={event => setInput('name', event.target.value)}
+          style={styles.input}
+          value={formState.name}
+          placeholder="task"
+        />
+        <input
+          onChange={event => setInput('description', event.target.value)}
+          style={styles.input}
+          value={formState.description}
+          placeholder="desc."
+        />
+        <button style={styles.button} onClick={addTodo}>Create Todo</button>
+        {
+          todos.map((todo, index) => (
+            <div key={todo.id ? todo.id : index} style={styles.todo}>
+              <p style={styles.todoName}>{todo.name}</p>
+              <p style={styles.todoDescription}>{todo.description}</p>
+            </div>
+          ))
+        }
+    </div>
+  
+  
   );
 }
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
   todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ddd', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 20, fontWeight: 'bold' },
+  input: { border: 'none', backgroundColor: '#ffffff', marginBottom: 10, padding: 8, fontSize: 18 },
+  todoName: { fontSize: 30, fontWeight: 'bold' },
   todoDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
-export default App
+export default withAuthenticator(App)
