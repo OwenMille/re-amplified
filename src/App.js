c7a1913a86d48f538b23d76e4c42424dcc85490d
OwenMille <90118261+OwenMille@users.noreply.github.com>
2022-06-03 19:33:54 -0400

    GraphQL models

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
-/* src/App.js */
-import React, { useEffect, useState } from 'react'
-import Amplify, { API, graphqlOperation } from 'aws-amplify'
+import Amplify from 'aws-amplify'
 import awsExports from "./aws-exports";
 import { withAuthenticator, Button, Heading } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import './App.css'
 
+
 Amplify.configure(awsExports);
-const initialState = { name: '', description: '' }
 
 function App({ signOut, user }) {
   return (
@@ -21,10 +19,6 @@ function App({ signOut, user }) {
 
 const styles = {
   container: { width: 400, margin: '0 auto', display: 'flex', flexDirection: 'column', justifyContent: 'center', padding: 20 },
-  todo: {  marginBottom: 15 },
-  input: { border: 'none', backgroundColor: '#ffffff', marginBottom: 10, padding: 8, fontSize: 18 },
-  todoName: { fontSize: 30, fontWeight: 'bold' },
-  todoDescription: { marginBottom: 0 },
   button: { backgroundColor: 'black', color: 'white', outline: 'none', fontSize: 18, padding: '12px 0px' }
 }
 
